b03157226ba46084f70c35927be653ff16db6dba
lpm <lpm@DESKTOP-OCO7AH1>
2021-08-30 00:34:42 +0200

    v0.0.1

---
+++ hollowsaves/Hollowsaves.Designer.cs
@@ -0,0 +1,230 @@
+﻿
+using System;
+using System.IO;
+
+namespace hollowsaves
+{
+	partial class Hollowsaves
+    {
+		/// <summary>
+		/// Variable del diseñador necesaria.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Limpiar los recursos que se estén usando.
+		/// </summary>
+		/// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Código generado por el Diseñador de Windows Forms
+
+		/// <summary>
+		/// Método necesario para admitir el Diseñador. No se puede modificar
+		/// el contenido de este método con el editor de código.
+		/// </summary>
+		private void InitializeComponent()
+		{
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Hollowsaves));
+            this.originPath = new System.Windows.Forms.Button();
+            this.originPathTextBox = new System.Windows.Forms.TextBox();
+            this.destinyPathTextBox = new System.Windows.Forms.TextBox();
+            this.nameTextBox = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.generateSaveButton = new System.Windows.Forms.Button();
+            this.restoreSaveButton = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.restoreButtonPathButton = new System.Windows.Forms.Button();
+            this.comboBox2 = new System.Windows.Forms.ComboBox();
+            this.destinyPathButton = new System.Windows.Forms.Button();
+            this.label6 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // originPath
+            // 
+            this.originPath.Image = ((System.Drawing.Image)(resources.GetObject("originPath.Image")));
+            this.originPath.Location = new System.Drawing.Point(468, 20);
+            this.originPath.Name = "originPath";
+            this.originPath.Size = new System.Drawing.Size(27, 22);
+            this.originPath.TabIndex = 3;
+            this.originPath.UseVisualStyleBackColor = true;
+            this.originPath.Click += new System.EventHandler(this.originPath_Click);
+            // 
+            // originPathTextBox
+            // 
+            this.originPathTextBox.Location = new System.Drawing.Point(129, 21);
+            this.originPathTextBox.Name = "originPathTextBox";
+            this.originPathTextBox.Size = new System.Drawing.Size(339, 20);
+            this.originPathTextBox.TabIndex = 4;
+            this.originPathTextBox.Text = "C:\\Users\\lpm\\AppData\\LocalLow\\Team Cherry\\Hollow Knight";
+            // 
+            // destinyPathTextBox
+            // 
+            this.destinyPathTextBox.Location = new System.Drawing.Point(129, 60);
+            this.destinyPathTextBox.Name = "destinyPathTextBox";
+            this.destinyPathTextBox.Size = new System.Drawing.Size(339, 20);
+            this.destinyPathTextBox.TabIndex = 5;
+            this.destinyPathTextBox.Text = "D:\\Users\\lpm\\Documents\\saves";
+            this.destinyPathTextBox.TextChanged += new System.EventHandler(this.destinyPathTextBox_TextChanged);
+            // 
+            // nameTextBox
+            // 
+            this.nameTextBox.Location = new System.Drawing.Point(129, 98);
+            this.nameTextBox.Name = "nameTextBox";
+            this.nameTextBox.Size = new System.Drawing.Size(339, 20);
+            this.nameTextBox.TabIndex = 8;
+            this.nameTextBox.TextChanged += new System.EventHandler(this.nameTextBox_TextChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 25);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(100, 13);
+            this.label1.TabIndex = 9;
+            this.label1.Text = "HK AppData Saves";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 63);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(84, 13);
+            this.label2.TabIndex = 10;
+            this.label2.Text = "Your saves path";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 101);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(107, 13);
+            this.label3.TabIndex = 11;
+            this.label3.Text = "Save name (optional)";
+            this.label3.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // generateSaveButton
+            // 
+            this.generateSaveButton.Location = new System.Drawing.Point(324, 124);
+            this.generateSaveButton.Name = "generateSaveButton";
+            this.generateSaveButton.Size = new System.Drawing.Size(144, 23);
+            this.generateSaveButton.TabIndex = 12;
+            this.generateSaveButton.Text = "Generate save";
+            this.generateSaveButton.UseVisualStyleBackColor = true;
+            this.generateSaveButton.Click += new System.EventHandler(this.generateSaveButton_Click);
+            // 
+            // restoreSaveButton
+            // 
+            this.restoreSaveButton.Location = new System.Drawing.Point(324, 206);
+            this.restoreSaveButton.Name = "restoreSaveButton";
+            this.restoreSaveButton.Size = new System.Drawing.Size(144, 23);
+            this.restoreSaveButton.TabIndex = 14;
+            this.restoreSaveButton.Text = "Restore selected save";
+            this.restoreSaveButton.UseVisualStyleBackColor = true;
+            this.restoreSaveButton.Click += new System.EventHandler(this.restoreSaveButtonClick);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(12, 184);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(0, 13);
+            this.label4.TabIndex = 15;
+            this.label4.Click += new System.EventHandler(this.label4_Click);
+            // 
+            // restoreButtonPathButton
+            // 
+            this.restoreButtonPathButton.Image = ((System.Drawing.Image)(resources.GetObject("restoreButtonPathButton.Image")));
+            this.restoreButtonPathButton.Location = new System.Drawing.Point(468, 178);
+            this.restoreButtonPathButton.Name = "restoreButtonPathButton";
+            this.restoreButtonPathButton.Size = new System.Drawing.Size(27, 23);
+            this.restoreButtonPathButton.TabIndex = 16;
+            this.restoreButtonPathButton.UseVisualStyleBackColor = true;
+            this.restoreButtonPathButton.Click += new System.EventHandler(this.restoreButtonPathButton_Click);
+            // 
+            // comboBox2
+            // 
+            this.comboBox2.FormattingEnabled = true;
+            this.comboBox2.Location = new System.Drawing.Point(129, 179);
+            this.comboBox2.Name = "comboBox2";
+            this.comboBox2.Size = new System.Drawing.Size(339, 21);
+            this.comboBox2.TabIndex = 18;
+            this.comboBox2.SelectedIndexChanged += new System.EventHandler(this.comboBox2_SelectedIndexChanged);
+            // 
+            // destinyPathButton
+            // 
+            this.destinyPathButton.Image = ((System.Drawing.Image)(resources.GetObject("destinyPathButton.Image")));
+            this.destinyPathButton.Location = new System.Drawing.Point(468, 59);
+            this.destinyPathButton.Name = "destinyPathButton";
+            this.destinyPathButton.Size = new System.Drawing.Size(27, 22);
+            this.destinyPathButton.TabIndex = 19;
+            this.destinyPathButton.UseVisualStyleBackColor = true;
+            this.destinyPathButton.Click += new System.EventHandler(this.destinyPathButton_Click_1);
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(12, 182);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(84, 13);
+            this.label6.TabIndex = 21;
+            this.label6.Text = "Your saves path";
+            // 
+            // Hollowsaves
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(513, 253);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.destinyPathButton);
+            this.Controls.Add(this.comboBox2);
+            this.Controls.Add(this.restoreButtonPathButton);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.restoreSaveButton);
+            this.Controls.Add(this.generateSaveButton);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.nameTextBox);
+            this.Controls.Add(this.destinyPathTextBox);
+            this.Controls.Add(this.originPathTextBox);
+            this.Controls.Add(this.originPath);
+            this.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Hollowsaves";
+            this.Text = "HK Saves";
+            this.Load += new System.EventHandler(this.Hollowsaves_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+		}
+
+		#endregion
+		private System.Windows.Forms.Button originPath;
+        private System.Windows.Forms.TextBox originPathTextBox;
+        private System.Windows.Forms.TextBox destinyPathTextBox;
+        private System.Windows.Forms.TextBox nameTextBox;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button generateSaveButton;
+        private System.Windows.Forms.Button restoreSaveButton;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button restoreButtonPathButton;
+        private System.Windows.Forms.ComboBox comboBox2;
+        private System.Windows.Forms.Button destinyPathButton;
+        private System.Windows.Forms.Label label6;
+    }
+}
+
---
+++ hollowsaves/Hollowsaves.cs
@@ -0,0 +1,227 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace hollowsaves
+{
+	public partial class Hollowsaves : Form
+	{
+		public Hollowsaves()
+		{
+			InitializeComponent();
+		}
+
+		private void textBox1_TextChanged(object sender, EventArgs e)
+		{
+
+		}
+
+		private void folderBrowserDialog1_HelpRequest(object sender, EventArgs e)
+		{
+
+		}
+
+		private void originPath_Click(object sender, EventArgs e)
+		{
+			FolderBrowserDialog fbd = new FolderBrowserDialog();
+			fbd.RootFolder = Environment.SpecialFolder.UserProfile;
+			fbd.Description = "Select Hollow Knight saves folder path";
+			if (fbd.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+				originPathTextBox.Text = fbd.SelectedPath;
+		}
+
+		private void textBox3_TextChanged(object sender, EventArgs e)
+		{
+
+		}
+
+		private void button2_Click(object sender, EventArgs e)
+		{
+
+		}
+
+		private void button1_Click(object sender, EventArgs e)
+		{
+
+		}
+
+		private void textBox4_TextChanged(object sender, EventArgs e)
+		{
+
+		}
+
+		private void button3_Click(object sender, EventArgs e)
+		{
+
+		}
+
+        private void textBox1_TextChanged_1(object sender, EventArgs e)
+        {
+
+        }
+
+        private void destinyPathButton_Click(object sender, EventArgs e)
+        {
+			FolderBrowserDialog fbd = new FolderBrowserDialog();
+			fbd.RootFolder = Environment.SpecialFolder.UserProfile;
+			fbd.Description = "Select the folder where saves will be create";
+			if (fbd.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+				destinyPathTextBox.Text = fbd.SelectedPath;
+		}
+
+    
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void generateSaveButton_Click(object sender, EventArgs e)
+        {
+			if (originPathTextBox.Text == "")
+            {
+				MessageBox.Show("Select HK AppData saves path");
+				return;
+			}
+
+			string[] datFiles = Directory.GetFiles(originPathTextBox.Text, "*.dat");
+
+			if(datFiles.Length == 0)
+			{
+				MessageBox.Show("Not .dat files found in \"" + originPathTextBox.Text + "\"");
+				return;
+			}
+
+			string saveName = nameTextBox.Text;
+
+			if (saveName.Equals(""))
+            {
+				saveName = "save_" + DateTime.UtcNow.ToString("yyyy-MM-ddTHH_mm_ss");
+
+			}
+
+			if (destinyPathTextBox.Text == "")
+			{
+				MessageBox.Show("Select your saves path");
+				return;
+			}
+
+			string path = String.Concat(destinyPathTextBox.Text, "\\", saveName);
+
+			if (!Directory.Exists(path))
+			{
+				DirectoryInfo di = Directory.CreateDirectory(path);
+			}
+
+			foreach (string f in datFiles)
+            {
+				string fName = f.Substring(originPathTextBox.Text.Length + 1);
+				File.Copy(Path.Combine(originPathTextBox.Text, fName), Path.Combine(path, fName), true);
+			}
+
+			MessageBox.Show("Save \"" + saveName + "\" file generated!");
+		}
+
+        private void toRestoreTextBox_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void restoreSaveButtonClick(object sender, EventArgs e)
+        {
+
+			if (comboBox2.SelectedItem== null) {
+				MessageBox.Show("Select a save to restore");
+				return;
+            }
+
+			string[] datFiles = Directory.GetFiles(comboBox2.SelectedItem.ToString(), "*.dat");
+
+			if (datFiles.Length == 0)
+			{
+				MessageBox.Show("Not .dat files found in \"" + comboBox2.SelectedItem.ToString() + "\"");
+				return;
+			}
+
+			foreach (string f in datFiles)
+			{
+				string fName = f.Substring(comboBox2.SelectedItem.ToString().Length + 1);
+				File.Copy(Path.Combine(comboBox2.SelectedItem.ToString(), fName), Path.Combine(originPathTextBox.Text, fName), true);			
+			}
+
+			MessageBox.Show("Save \"" + comboBox2.SelectedItem.ToString() + "\" restored!");
+		}
+
+        private void nameTextBox_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+
+        private void restoreButtonPathButton_Click(object sender, EventArgs e)
+        {
+			FolderBrowserDialog fbd = new FolderBrowserDialog();
+			fbd.RootFolder = Environment.SpecialFolder.UserProfile;
+			fbd.Description = "Select Hollow Knight save files path";
+			if (fbd.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+				destinyPathTextBox.Text = fbd.SelectedPath;
+
+			if (fbd.SelectedPath != "")
+            {
+				string[] dirs = Directory.GetDirectories(fbd.SelectedPath);
+
+				foreach (string dir in dirs)
+				{
+					comboBox2.Items.Add(dir);
+				}
+			}
+
+		}
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void destinyPathButton_Click_1(object sender, EventArgs e)
+        {
+			FolderBrowserDialog fbd = new FolderBrowserDialog();
+			fbd.RootFolder = Environment.SpecialFolder.UserProfile;
+			fbd.Description = "Select the folder where saves will be create";
+			if (fbd.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+				destinyPathTextBox.Text = fbd.SelectedPath;
+		}
+
+        private void destinyPathTextBox_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void comboBox2_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Hollowsaves_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
